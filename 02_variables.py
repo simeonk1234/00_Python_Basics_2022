10b40e2ea2dc0af78f890f4326dbc2fe21d55f67
simeonk1234 <101219115+simeonk1234@users.noreply.github.com>
2022-03-21 12:56:22 +1300

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,15 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 7
+print(fav_num * 2)
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14 
+
+-01_variable = "try this"
++num_2 = 2
+
